e29d23bcbb264e74682ba65bc4226cc4c269461d
hongsheng <1634340724@qq.com>
2022-10-21 04:30:14 +0800

    注册中心

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private RestTemplate restTemplate;
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        //2发送http请求查询用户
+        //获取发送路径
+        String url = "http://userservice/user/"+order.getUserId();
+        //发送请求 实现远程调用
+        User user = restTemplate.getForObject(url, User.class);
+        //封装user到order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
+
